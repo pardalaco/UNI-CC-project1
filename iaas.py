8f18086bc97c130e5137d04e3a5a878f3d41bddb
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-12-06 15:02:23 +0100

    iaas removeVm

--- iaas.py
+++ iaas.py
@@ -637,3 +637,41 @@ def removeVm(token:str, vmId:str):
     permisos existentes sobre la vm.
     """
     print("iaas.removeVm()")
+    issuer = validateToken(token)
+
+    # Comprobamos si existe la vm
+    db = sqlite3.connect(FILE_DB)
+    cur = db.cursor()
+    try:
+        cur.execute(f"""SELECT 1 FROM Vms WHERE id = '{vmId}';""")
+        rows = cur.fetchall()
+        if rows == []: raise Exception("vm don't exist")
+    except Exception as e:
+        traceback.print_exc()
+        raise e
+    finally: 
+        db.commit()
+        db.close()
+
+    # Verificamos los permisos y ejecutamos la función
+    if issuer["admin"]:
+        core.removeVm(vmId)
+    else:
+        db = sqlite3.connect(FILE_DB)
+        cur = db.cursor()
+        try:
+            cur.execute(f"""SELECT * FROM perms WHERE 
+                        user = '{issuer['id']}' and
+                        resource = '{vmId}' and
+                        type = 'vm';
+                        """)
+            rows = cur.fetchall()
+            if rows == []: raise Exception("You do not have permissions")
+        except Exception as e:
+            traceback.print_exc()
+            raise e
+        finally: 
+            db.commit()
+            db.close()
+        
+        core.removeVm(vmId)
